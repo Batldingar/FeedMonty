472ff83925b13c6963be2d305ed0fddebf55c0a6
Batldingar <baldinger.andreas@gmail.com>
2021-10-13 21:38:44 +0200

    Minor changes

--- app/src/main/java/com/baldware/feedmonty/PrivacyDialogFragment.java
+++ app/src/main/java/com/baldware/feedmonty/PrivacyDialogFragment.java
@@ -34,14 +34,14 @@ public class PrivacyDialogFragment extends DialogFragment {
             title = getArguments().getString("title");
         }
 
-        LayoutInflater layoutInflater = Objects.requireNonNull(getActivity()).getLayoutInflater();
+        LayoutInflater layoutInflater = getActivity().getLayoutInflater();
         View dialogView = layoutInflater.inflate(R.layout.fragment_privacy_policy, null);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(title)
                 .setView(dialogView)
                 .setPositiveButton(R.string.privacy_policy_acceptance_text, (dialog, which) -> {
-                    HistoryHandler historyHandler = new HistoryHandler(Objects.requireNonNull(PrivacyDialogFragment.this.getContext()), MainActivity.HISTORY_FILE_TAG);
+                    HistoryHandler historyHandler = new HistoryHandler(PrivacyDialogFragment.this.getContext(), MainActivity.HISTORY_FILE_TAG);
                     historyHandler.writeHistory(MainActivity.PRIVACY_POLICY_KEY, "true", HistoryHandler.Category.SETTINGS);
                 });
 
